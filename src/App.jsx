8cf74a46383a004e4d6dbddc43947d07abdc0a6d
Samuel <frostrik@gmail.com>
2024-02-07 09:03:31 -0600

    Added basic Structure and Header and Fotter component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './App.css'
+import {
+  Route,
+  Routes
+} from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+     <Header />
+     <Routes> 
+    </Routes>
+     <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Nav from "./Nav"
+import { Link } from "react-router-dom"
+export default function Header() {
+
+    return (
+      <header>
+        <Link to="/"><img src="/images/logo.png" alt="little lemon"/></Link>
+        <Nav/>
+      </header>
+    )
+  }
